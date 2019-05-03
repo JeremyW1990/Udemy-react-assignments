540dc25548e709406f3bce12db33c6792bb0f445
CJ Wang <shijie.wang1990@gmail.com>
2019-05-02 21:37:51 -0700

    Redirect requests to /all-courses to /courses (=> Your Courses page)

--- routing--assignment-problem/src/App.js
+++ routing--assignment-problem/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Route, NavLink, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, NavLink, Switch, Redirect } from 'react-router-dom';
 
 import Courses from './containers/Courses/Courses';
 import Users from './containers/Users/Users';
@@ -27,6 +27,7 @@ class App extends Component {
           <Switch>
             <Route path ='/users' exact component={Users}/>
             <Route path ='/courses' component={Courses}/>  
+            <Redirect from='/all-courses' to='/courses'/>
             <Route render = {() => <h1>PAGE NOT FOUND</h1>}/>
           </Switch>  
         </div>
